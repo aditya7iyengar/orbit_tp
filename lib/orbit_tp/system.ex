969e8bff9e7b8837fb86d5b89579582701e8a5ee
Adi <aditya7iyengar@gmail.com>
2017-12-20 09:44:27 -0500

    WIP:
    - Add system

--- lib/orbit_tp/system.ex
+++ lib/orbit_tp/system.ex
@@ -1,3 +1,24 @@
 defmodule OrbitTp.System do
-  use GenServer
+  use Agent
+
+  alias OrbitTp.{Body, Gravity}
+
+  def init(body1, body2) do
+    Agent.start_link(fn -> {body1, body2} end, name: __MODULE__)
+  end
+
+  def get_state(time) do
+  end
+
+  defp apply_components(components, time) do
+    Enum.each(components, &apply_component(&1, time))
+  end
+
+  defp apply_component(Gravity, time) do
+    {body1, body2} = Agent.get(__MODULE__, & &1)
+  end
+
+  defp get_distance(body1, body2) do
+
+  end
 end
